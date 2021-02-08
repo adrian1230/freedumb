c2cd5fd1f85d135410758f6440d70c46f2df6aef
adrian1230 <adrian1145d98@gmail.com>
2021-02-09 01:49:26 +0800

    adrian

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -97,7 +97,7 @@ class _HomeState extends State<Home> {
                         children: [
                           for (var i in books)
                             Container(
-                              padding: EdgeInsets.symmetric(horizontal: size.width * 0.05, vertical: size.height * 0.03),
+                              padding: EdgeInsets.symmetric(horizontal: size.width * 0.02, vertical: size.height * 0.01),
                               margin: EdgeInsets.only(bottom: 16),
                               width: size.width - 38,
                               decoration: BoxDecoration(
