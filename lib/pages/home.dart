7e5446ab1d02ee1864b943f27d38f45983c543e0
adrian1230 <adrian1145d98@gmail.com>
2021-02-09 22:23:02 +0800

    adrian

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -71,7 +71,7 @@ class _HomeState extends State<Home> {
             ),
             title: Text(i["name"]),
             subtitle: Text(i["price"].toString()+" USD"),
-            onLongPress: () => {},
+            onTap: () => {},
           ),
         ),
     ];
